036c1492a8eb3f5e64d37444901ab477105a4855
vyshiva <elena_haponyuk@ukr.net>
2018-01-21 12:38:05 +0200

    New page - projects or Portfolio Page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
+//const port = 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -47,6 +48,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects',(req,res)=>{
+  res.render('projects',{
+    pageTitle: 'Portfolio Page'
   });
 });
 
